c2e8684bf46c23cbed5494223c870568436d3a71
IT-lam1ch <la.max.act@gmail.com>
2024-12-04 22:10:32 +0300

    Initial commit

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', home, name='home')
+]
+urlpatterns += staticfiles_urlpatterns()
